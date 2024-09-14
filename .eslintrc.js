0bebba98b93bef93c7951c565f717dba5ef01d4f
JairGuzman1810 <jair18101@hotmail.com>
2024-09-13 22:09:18 -0600

    eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+// https://docs.expo.dev/guides/using-eslint/
+module.exports = {
+  extends: ["expo", "prettier"],
+  plugins: ["prettier", "react-hooks", "react-native"], // Add "react-native" plugin
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+        trailingComma: "es5", // Ensure this matches your intended style
+      },
+    ],
+    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+    "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies
+    "react-native/no-unused-styles": "warn", // Warn for unused styles
+  },
+};
